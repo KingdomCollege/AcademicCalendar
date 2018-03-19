407f0f361c61d86afe6d72b1c858d8dd3a0fca2e
marcwebdude <marcus.faye@yahoo.com>
2018-03-19 10:29:12 -0500

    updated for dual-view

--- js/local.js
+++ js/local.js
@@ -1,16 +1,24 @@
 $(function () {
-    $('#calendar').fullCalendar({
+    $('#calendarView').fullCalendar({
         header: {
             left: 'prev,next today',
             center: 'title',
-            right: 'month,listYear'
+            right: 'month,basicWeek,agendaDay'
         },
         displayEventTime: false,
         googleCalendarApiKey: 'AIzaSyCvfqhG0sp2h9YGStl9B0Lx46QAMEPgDpI',
         events: 'kingdomcollege.net_4nj2njko8q2otfrb0jkc2v2ggo@group.calendar.google.com',
+        defaultView: 'month',
         eventClick: function (event) {
             window.open(event.url, '_blank', 'width=700,height=600');
             return false;
         }
+    });
+    
+    $('#calendarList').fullCalendar({
+        displayEventTime: false,
+        googleCalendarApiKey: 'AIzaSyCvfqhG0sp2h9YGStl9B0Lx46QAMEPgDpI',
+        events: 'kingdomcollege.net_4nj2njko8q2otfrb0jkc2v2ggo@group.calendar.google.com',
+        defaultView: 'listYear'
     });
 });
